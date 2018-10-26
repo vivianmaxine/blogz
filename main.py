e995ce9534b66d6975b8ba987987bdf6911e4613
Vivian Crosby <design@vivianmaxine.com>
2018-10-26 04:47:30 -0500

    Separated index.html and blog.html initially to differentiate them

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -10,39 +10,102 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)  # creates database object
 
+app.secret_key = '&VAptc8mjJj^CL$S'
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blog_posts = db.relationship('BlogPost', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     entry = db.Column(db.String(50000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, entry):
+    def __init__(self, title, entry, owner):
         self.title = title
         self.entry = entry
+        self.owner = owner
 
     def __repr__(self):
         return '<BlogPost %r>' % self.title
 
 
-@app.route('/blog', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def blog():
+    owner = User.query.filter_by(email=session['email']).first()
+
     if request.method == 'POST':
         post_title = request.form['title']
         post_entry = request.form['entry']
-
-        new_post = BlogPost(title=post_title, entry=post_entry)
-
+        new_post = BlogPost(
+            title=post_title, entry=post_entry, owner=owner)
         db.session.add(new_post)
         db.session.commit()
 
-    posts = BlogPost.query.all()
+    posts = BlogPost.query.filter_by(owner=owner).all()
 
-    return render_template('index.html', title="Vivian's Blog", posts=posts)
+
+    return render_template('index.html', title="Blogz", posts=posts)
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify_pw = request.form['verify']
+        # TODO: VALIDATE THE DATA
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            # TODO: REMEMBER THE USER
+            session['email'] = email
+            return redirect('/')
+        else:
+            return "<h1>User Already Exists! :(</h1>"
+    return render_template('register.html')
+
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            # TODO: REMEMBER USER LOGGED IN
+            session['email'] = email
+            # TODO: ADD VALIDATION
+            flash("Success! You are Logged in.", 'success')
+            return redirect('/')
+        else:
+            # TODO: TELL THEM WHY LOGIN FAILED
+            flash("Incorrect password or user does not exist", 'error')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -53,6 +116,7 @@ def create_post():
     if request.method == 'POST':
         new_post_title = request.form['new_title']
         new_post_entry = request.form['new_entry']
+        owner = User.query.filter_by(email=session['email']).first()
 
         if new_post_title == '':
             title_error = 'Please enter a title for your blog post.'
@@ -61,7 +125,7 @@ def create_post():
             entry_error = 'Please enter content for your blog post.'
 
         if title_error == '' and entry_error == '':
-            new_post = BlogPost(title=new_post_title, entry=new_post_entry)
+            new_post = BlogPost(title=new_post_title, entry=new_post_entry, owner=owner)
 
             db.session.add(new_post)
             db.session.commit()
