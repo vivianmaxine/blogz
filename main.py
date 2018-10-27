3f35f2062b5901edbf626a83834516be78c28f89
Vivian Crosby <design@vivianmaxine.com>
2018-10-26 22:39:11 -0500

    Updated the registration handler and template to include validation using flash messages

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ app.secret_key = '&VAptc8mjJj^CL$S'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     blog_posts = db.relationship('BlogPost', backref='owner')
 
-    def __init__(self, email, password):
+    def __init__(self, username, email, password):
+        self.username = username
         self.email = email
         self.password = password
 
@@ -40,6 +42,23 @@ class BlogPost(db.Model):
 
 
 @app.route('/', methods=['POST', 'GET'])
+def index():
+    owner = User.query.filter_by(email=session['email']).first()
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_entry = request.form['entry']
+        new_post = BlogPost(
+            title=post_title, entry=post_entry, owner=owner)
+        db.session.add(new_post)
+        db.session.commit()
+
+    posts = BlogPost.query.filter_by(owner=owner).all()
+
+    return render_template('index.html', title="Blogz | Home", posts=posts)
+
+
+@app.route('/blog', methods=['POST', 'GET'])
 def blog():
     owner = User.query.filter_by(email=session['email']).first()
 
@@ -53,27 +72,47 @@ def blog():
 
     posts = BlogPost.query.filter_by(owner=owner).all()
 
-
-    return render_template('index.html', title="Blogz", posts=posts)
+    return render_template('blog.html', title="Blogz", posts=posts)
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
+        username = request.form['username']
         email = request.form['email']
         password = request.form['password']
         verify_pw = request.form['verify']
-        # TODO: VALIDATE THE DATA
-        existing_user = User.query.filter_by(email=email).first()
-        if not existing_user:
-            new_user = User(email, password)
-            db.session.add(new_user)
-            db.session.commit()
-            # TODO: REMEMBER THE USER
-            session['email'] = email
-            return redirect('/')
+
+    # TODO: VALIDATE THE DATA
+
+        if username == '' or email == '' or password == '' or verify_pw == '':
+            flash("Please fill in all form fields.", 'error')
         else:
-            return "<h1>User Already Exists! :(</h1>"
+            if len(username) > 2 and len(password) > 2:
+                if password == verify_pw:
+                    existing_user = User.query.filter_by(
+                        username=username).first()
+                    existing_user_email = User.query.filter_by(
+                        email=email).first()
+                    if not existing_user and not existing_user_email:
+                        new_user = User(username, email, password)
+                        db.session.add(new_user)
+                        db.session.commit()
+                        # TODO: REMEMBER THE USER
+                        session['username'] = username
+                        flash(
+                            "Congratulations! You have successfully created an account!\
+                            Log in below.", 'success')
+                        return redirect('/')
+                    else:
+                        flash("Username or email already exists.", 'error')
+                else:
+                    flash("Passwords do not match.", 'error')
+            else:
+                flash(
+                    "Username and password must be at least 3 \
+                    characters long.", 'error')
+
     return render_template('register.html')
 
 
@@ -87,12 +126,12 @@ def require_login():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
         if user and user.password == password:
             # TODO: REMEMBER USER LOGGED IN
-            session['email'] = email
+            session['username'] = username
             # TODO: ADD VALIDATION
             flash("Success! You are Logged in.", 'success')
             return redirect('/')
@@ -125,7 +164,8 @@ def create_post():
             entry_error = 'Please enter content for your blog post.'
 
         if title_error == '' and entry_error == '':
-            new_post = BlogPost(title=new_post_title, entry=new_post_entry, owner=owner)
+            new_post = BlogPost(
+                title=new_post_title, entry=new_post_entry, owner=owner)
 
             db.session.add(new_post)
             db.session.commit()
